3cf9a5b2c21e04f2408417c6cfafebf04778c873
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-12 08:20:45 +0430

    how to define and call functions in python

---
+++ Day 6/definingAndCallingFucntions.py
@@ -0,0 +1,35 @@
+# Defining the function starting with the def keyword which means define with a parameter
+def say_my_name(name):
+    print(f"Your name is {name}")
+
+
+# Calling the function with a argument
+say_my_name("Ali")
+
+
+# Defining the function without any parameters
+def my_function():
+    print("Hello")
+    print("Good Bye")
+
+
+# Calling the function
+my_function()
+
+
+# the turn_righ() function
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# Drawing a square in Reeborg's world webist
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()

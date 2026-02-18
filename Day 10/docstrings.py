7ab9f877bf2281184360f45eb971446a88dc373c
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-18 09:26:07 +0430

    Docstrings are used to add details and explanation to functions when its called

---
+++ Day 10/docstrings.py
@@ -0,0 +1,16 @@
+# Docstrings are used for our code documentation of our code or give explanation to the code
+# it should come the first of line after declaration
+# Function with multiple return values.
+
+def format_name(f_name, l_name):
+    """Take the first and last name and format it
+    to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+formated_string = format_name(input("What is your first name? "),input("What is your last name? "))
+print(formated_string)
+

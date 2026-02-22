a0e6b6da9c201a5ed59e6b50f427c439f96ff21a
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-22 09:11:00 +0430

    Solving the bugs of the odd or even number practice project

---
+++ Day 13/debugginOddOrEven.py
@@ -0,0 +1,10 @@
+# Read the code in main.py
+# Spot the problems.
+# Modify the code to fix the program
+# Fix the codes so that it works and passes the tests when you submit.
+# HINT: Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+number = int(input("Which number do you want to check? "))
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number")

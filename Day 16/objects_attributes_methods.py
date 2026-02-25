722639323365025303f430d5318d160ee200e852
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-25 09:04:57 +0430

    Importing modules, constructing objects, accessing attributes and methods.

---
+++ Day 16/objects_attributes_methods.py
@@ -0,0 +1,22 @@
+# import another_module
+# print(another_module.another_variable)
+
+
+#constructing objects
+from turtle import Turtle, Screen
+timmy = Turtle()
+#Object methods
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+
+#Oject attributes
+my_screen = Screen()
+print(my_screen.canvheight)
+
+#Object methods
+my_screen.exitonclick()
+
+
+

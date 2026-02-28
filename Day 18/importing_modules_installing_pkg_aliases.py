ae669a1f08baeb2aecc2167c73dc1862ef540b1b
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-02-28 08:36:45 +0430

    The ways of importing modules, installing packages, and using aliases

---
+++ Day 18/importing_modules_installing_pkg_aliases.py
@@ -0,0 +1,13 @@
+# Ways of importing modules
+#                     Kyword + Module name
+# 1- Basic  Import : import turtle
+#                  keyword+ModuleName+keyword+Thing in Module
+# 2- from...import: from turtle import Turtle
+#                          keyword+ModuleName+keyword+Everything
+# 3- importing everything:  from turtle import *
+
+# Aliasing Modules:
+# keyword + Module name + keyword + alias name
+# import  turtle as t
+
+# Installing Modules:

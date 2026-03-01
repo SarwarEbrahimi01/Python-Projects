85e378e013794eb8571e2bce268fdd3a5ec1db22
Sarwar Ebrahimi <sarwarebrahimi01@gmail.com>
2026-03-01 08:48:56 +0430

    functions that accepts inputs

---
+++ Day 19/functions_as_inputs.py
@@ -0,0 +1,11 @@
+# def function_a(something):
+#     # Do this with something
+#     # Then do this
+#     # Finally do this
+
+# def function_b():
+#     #Do this
+
+# # Passing function_b to the function_a
+# This is called Higher Order Function in python
+# function_a(function_b)
